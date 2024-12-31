8122a3d8c40bc8d6a61e97a9eff904a22177cc83
satiparpar <mohammad.arpar.99@gmail.com>
2024-12-31 19:39:44 +0330

    new [Raffle]: Implement error and event for "enterRaffle" method

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,24 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
@@ -8,15 +29,18 @@ pragma solidity 0.8.19;
  * @dev Implements Chainlink VRFv2.5
  */
 contract Raffle {
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffel();
+
     uint256 private immutable i_entrancePrice;
+    address payable[] public s_players;
+
+    /* Events */
+    event RaffleEnterd(address indexed player);
 
     constructor(uint256 entrancePrice) {
         i_entrancePrice = entrancePrice;
     }
-
-    function enterRaffle() public payable {}
-
-    function pickWinner() public {}
 
     /**
      * Getter Functions
@@ -24,4 +48,14 @@ contract Raffle {
     function getEntranceFee() external view returns (uint256) {
         return i_entrancePrice;
     }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entrancePrice) {
+            revert Raffle__SendMoreToEnterRaffel();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnterd(msg.sender);
+    }
+
+    function pickWinner() public {}
 }
