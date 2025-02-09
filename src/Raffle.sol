4d4dcebd8c1accd44c9fdb6d0c0a14934e9eedd9
satiparpar <mohammad.parpar.99@gmail.com>
2025-02-09 14:46:51 +0330

    update[Raffle]: pick winner using random number

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -34,6 +34,7 @@ import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/V
 contract Raffle is VRFConsumerBaseV2Plus {
     /* Errors */
     error Raffle__SendMoreToEnterRaffel();
+    error Raffle__TransferFailed();
 
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
@@ -45,6 +46,7 @@ contract Raffle is VRFConsumerBaseV2Plus {
     uint32 private immutable i_callbackGasLimit;
     uint256 private s_lastTimeStampe;
     address payable[] public s_players;
+    address private s_recentWinner;
 
     /* Events */
     event RaffleEnterd(address indexed player);
@@ -102,5 +104,13 @@ contract Raffle is VRFConsumerBaseV2Plus {
     function fulfillRandomWords(
         uint256 requestId,
         uint256[] calldata randomWords
-    ) internal override {}
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+    }
 }
